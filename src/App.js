d6f0c7b920c7eb1edf464b91d4ad5d93782caf68
jemo <1321332562@qq.com>
2018-06-26 13:46:17 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import 'ant-design-pro/dist/ant-design-pro.css'; 
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default App;
